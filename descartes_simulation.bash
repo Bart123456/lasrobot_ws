c98b922438c07efe1a47130a88cc051030084b75
Bart123456 <bart.moyaers@student.uhasselt.be>
2017-02-23 12:23:43 +0100

    something

---
+++ descartes_simulation.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ~/lasrobot_ws/devel/setup.bash
+roslaunch descartes_tutorials tutorial1.launch
---
+++ rviz.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ~/lasrobot_ws/devel/setup.bash
+roslaunch lasrobot_moveit_generated descartes_pathplanning.launch
