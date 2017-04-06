788ccd1db1e92a341fce41e315194bd1aa8fd6b6
JeroenDM <jeroendemaeyer@live.be>
2017-04-06 13:50:58 +0200

    almost good figure

--- plots/plotAngles.py
+++ plots/plotAngles.py
@@ -1,46 +1,61 @@
 import rosbag
 import matplotlib.pyplot as plt
 
-# read bag files
+# read bag cylinder A
+# ===================
+bag = rosbag.Bag('test.bag')
+points = []
+for topic, msg, t in bag.read_messages(topics=['trajectory']):
+    points = msg.points
+bag.close()
+
+tA = []
+joint_positions = []
+for point in points:
+	tA.append(point.time_from_start.secs + point.time_from_start.nsecs / 1e9)
+	joint_positions.append(point.positions)
+
+jA = [[],[],[],[],[],[]]
+for joints in joint_positions:
+	for i in range(0, 6):
+		jA[i].append(joints[i])
+
+# read bag l profile B
+# ====================
 bag = rosbag.Bag('L_profile_without_weldingcost.bag')
 points = []
 for topic, msg, t in bag.read_messages(topics=['trajectory']):
     points = msg.points
 bag.close()
 
-time = []
+tA = []
 joint_positions = []
 for point in points:
-	time.append(point.time_from_start.secs + point.time_from_start.nsecs / 1e9)
+	tA.append(point.time_from_start.secs + point.time_from_start.nsecs / 1e9)
 	joint_positions.append(point.positions)
 
-joint1 = []
-joint2 = []
-joint3 = []
-joint4 = []
-joint5 = []
-joint6 = []
-for joints in joint_positions:
-	joint1.append(joints[0])
-	joint2.append(joints[1])
-	joint3.append(joints[2])
-	joint4.append(joints[3])
-	joint5.append(joints[4])
-	joint6.append(joints[5])
-
-#~ import matplotlib.pyplot as plt
-#~ plt.plot(time, joint1, 'b', time, joint2, 'g',time, joint3, 'r',time, joint4, 'c',time, joint5, 'm',time, joint6, 'y')
-#~ plt.ylabel('Joint angles [rad]')
-#~ plt.xlabel('Time [s]')
-#~ plt.legend(['Joint 1', 'Joint 2', 'Joint 3', 'Joint 4', 'Joint 5', 'Joint 6'])
-#~ plt.show()
-
-jwithout = [[],[],[],[],[],[]]
+jA = [[],[],[],[],[],[]]
 for joints in joint_positions:
 	for i in range(0, 6):
-		jwithout[i].append(joints[i])
+		jA[i].append(joints[i])
 
+# create plot
+# ===========
 plt.figure()
+plt.subplot(121)
 for i in range(0, 6):
-	plt.plot(time, jwithout[i])
-plt.show()
+	plt.plot(tA, jA[i])
+plt.title("Case A: cylinder")
+plt.ylabel('Joint angles [rad]')
+plt.xlabel('Time [s]')
+#~ plt.legend(['Joint 1', 'Joint 2', 'Joint 3', 'Joint 4', 'Joint 5', 'Joint 6'])
+
+plt.subplot(122)
+for i in range(0, 6):
+	plt.plot(tA, jA[i])
+plt.title("Case B: l-profile")
+plt.xlabel('Time [s]')
+plt.legend(['Joint 1', 'Joint 2', 'Joint 3', 'Joint 4', 'Joint 5', 'Joint 6'])
+
+plt.savefig("joint_angles_AB.png")
+#~ plt.show()
