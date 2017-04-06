8f4941eb9e882c0a8a23d52de734c34aa8ea9cc3
Bart123456 <bart.moyaers@student.uhasselt.be>
2017-04-06 13:15:45 +0200

    Added testcase files with plot

---
+++ plots/plotAngleErrors.py
@@ -0,0 +1,54 @@
+import rosbag
+bag = rosbag.Bag('L_profile_with_weldingcost.bag')
+errorsX = []
+errorsY = []
+points = []
+for topic, msg, t in bag.read_messages(topics=['angleErrorsX','angleErrorsY','trajectory']):
+	if topic == 'angleErrorsX':
+		errorsX = msg.data
+    	
+	if topic == 'angleErrorsY':
+		errorsY = msg.data
+    	
+	if topic == 'trajectory':
+		points = msg.points
+bag.close()
+
+bag2 = rosbag.Bag('L_profile_without_weldingcost.bag')
+errorsYnocost = []
+for topic, msg, t in bag2.read_messages(topics=['angleErrorsY']):
+	if topic == 'angleErrorsY':
+		errorsYnocost = msg.data
+bag2.close()
+
+time = []
+joint_positions = []
+for point in points:
+	time.append(point.time_from_start.secs + point.time_from_start.nsecs / 1e9)
+	joint_positions.append(point.positions)
+
+#remove 'NaN' values from lists (supposed to be 0)
+count = 0
+for error in errorsX:
+	if error == 'nan':
+		errorsX[count] = 0.0
+	count += 1
+	
+count = 0
+for error in errorsY:
+	if error == 'nan':
+		errorsY[count] = 0.0
+	count += 1
+	
+count = 0
+for error in errorsYnocost:
+	if error == 'nan':
+		errorsYnocost[count] = 0.0
+	count += 1
+	
+import matplotlib.pyplot as plt
+plt.plot(time, errorsY, 'b', time, errorsYnocost, 'g')
+plt.ylabel('Angle errors in radian')
+plt.xlabel('Time in seconds')
+plt.legend(['Y Angle Error with weldingcost [rad]', 'Y Angle Error no cost [rad]'])
+plt.show()
