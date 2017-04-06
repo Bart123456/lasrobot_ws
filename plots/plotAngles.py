9831079bcaadf199521b8fd6c4ede550af07aa14
JeroenDM <jeroendemaeyer@live.be>
2017-04-06 16:28:34 +0200

    update joint plot

--- plots/plotAngles.py
+++ plots/plotAngles.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # read bag cylinder A
 # ===================
-bag = rosbag.Bag('test.bag')
+bag = rosbag.Bag('cylinder_IKfast_100pt.bag')
 points = []
 for topic, msg, t in bag.read_messages(topics=['trajectory']):
     points = msg.points
@@ -19,29 +19,33 @@ jA = [[],[],[],[],[],[]]
 for joints in joint_positions:
 	for i in range(0, 6):
 		jA[i].append(joints[i])
+
+bag.close()
 
 # read bag l profile B
 # ====================
-bag = rosbag.Bag('L_profile_without_weldingcost.bag')
+bagB = rosbag.Bag('L_profile_without_weldingcost.bag')
 points = []
-for topic, msg, t in bag.read_messages(topics=['trajectory']):
+for topic, msg, t in bagB.read_messages(topics=['trajectory']):
     points = msg.points
 bag.close()
 
-tA = []
+tB = []
 joint_positions = []
 for point in points:
-	tA.append(point.time_from_start.secs + point.time_from_start.nsecs / 1e9)
+	tB.append(point.time_from_start.secs + point.time_from_start.nsecs / 1e9)
 	joint_positions.append(point.positions)
 
-jA = [[],[],[],[],[],[]]
+jB = [[],[],[],[],[],[]]
 for joints in joint_positions:
 	for i in range(0, 6):
-		jA[i].append(joints[i])
+		jB[i].append(joints[i])
+
+bagB.close()
 
 # create plot
 # ===========
-plt.figure()
+plt.figure(figsize=(8, 4))
 plt.subplot(121)
 for i in range(0, 6):
 	plt.plot(tA, jA[i])
@@ -52,7 +56,7 @@ plt.xlabel('Time [s]')
 
 plt.subplot(122)
 for i in range(0, 6):
-	plt.plot(tA, jA[i])
+	plt.plot(tB, jB[i])
 plt.title("Case B: l-profile")
 plt.xlabel('Time [s]')
 plt.legend(['Joint 1', 'Joint 2', 'Joint 3', 'Joint 4', 'Joint 5', 'Joint 6'])
