c7c8914e260ae7f23d8655286edae11dcbdd5b7b
JeroenDM <jeroendemaeyer@live.be>
2017-04-11 22:55:09 +0200

    active sparse planner and add tolerances

--- src/descartes_tutorials/src/robot.cpp
+++ src/descartes_tutorials/src/robot.cpp
@@ -16,7 +16,7 @@
 
 
 // Includes the planner we will be using
-#include <descartes_planner/dense_planner.h>
+#include <descartes_planner/sparse_planner.h>
 //Include visualization markers for RViz
 //#include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
@@ -245,12 +245,16 @@ int main(int argc, char** argv)
     trajPoses.poses[j] = tempPose;
   }
 
+  // *********************
   //Define tolerance sizes
+  // *********************
   trajectory.setRotStepSize(M_PI/180);
   double rxTolerance, ryTolerance, rzTolerance;
-  rxTolerance = M_PI/4; //M_PI/36;
-  ryTolerance = 0; //M_PI/36;
-  rzTolerance = 0;
+  rxTolerance = M_PI/4; // 45 sample points
+  ryTolerance = 0; //
+  rzTolerance = M_PI/20; // 18 sample points
+  // total of 45 * 18 = 810 sample points (which is a lot) too much ram
+  // total of 45 * 9 = 405 sample points (should work, 360 worked before)
 
   for(int i = 0; i < tempSize; ++i)
   {
@@ -325,13 +329,14 @@ int main(int argc, char** argv)
   //model->updateInternals();
 
   // 3. Create a planner and initialize it with our robot model
-  descartes_planner::DensePlanner planner;
+  descartes_planner::SparsePlanner planner;
   planner.initialize(model);
 
+  // TODO does not work yet with sparse planner
   //Use the extra weldingscost or not
-  planner.getPlanningGraph().setUseWeldingCost(useWeldingCost);
+  //planner.getPlanningGraph().setUseWeldingCost(useWeldingCost);
   //Set the weldingcost weight:
-  planner.getPlanningGraph().setWeldingCostFactor(weldingCostWeight);
+  //planner.getPlanningGraph().setWeldingCostFactor(weldingCostWeight);
 
   //Don't plan path if it is read from file.
   if(!readTrajectoryFile)
@@ -355,13 +360,14 @@ int main(int argc, char** argv)
       return -3;
     }
 
+    // TODO does not work yet with sparse planner
     //Extract weldingcosts from result (Don't forget to multiply with the welding weight!)
-    weldingCosts.data.resize(result.size());
-    double costWeight = planner.getPlanningGraph().getWeldingCostFactor();
-    for(int i = 0; i < result.size(); ++i)
-    {
-      weldingCosts.data[i] = result[i]->getWeldingCost() * costWeight;
-    }
+    // weldingCosts.data.resize(result.size());
+    // double costWeight = planner.getPlanningGraph().getWeldingCostFactor();
+    // for(int i = 0; i < result.size(); ++i)
+    // {
+    //   weldingCosts.data[i] = result[i]->getWeldingCost() * costWeight;
+    // }
     
 
     
